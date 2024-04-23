d50921c6515d97abb49678f89ae012dcb46f84c4
Балалаев Вячеслав <desent-1@mail.ru>
2024-04-23 23:48:59 +0300

    ыы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("ИЗменено");
+        System.out.println("Что то ещё");
+        System.out.println("One more");
+        System.out.println("И тебя тут нет");
     }
 }
